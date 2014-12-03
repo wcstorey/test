bae90bddc70909a12ab9462421b648a97ff7e612
William Storey <wcstorey@gmail.com>
2014-12-03 15:33:04 -0500

    working on clock

--- public/js/application.js
+++ public/js/application.js
@@ -3,42 +3,31 @@ $(document).ready(function() {
     $("#new_form").dialog({
       autoOpen: false
       });
-      $("#button").on("click", function() {
-      $("#dialog").dialog("open");
-      });
-      });
-      // Validating Form Fields.....
-      $("#submit").click(function(e) {
-      var email = $("#email").val();
-      var name = $("#name").val();
-      var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-      if (email === '' || name === '') {
-      alert("Please fill all fields...!!!!!!");
-      e.preventDefault();
-      } else if (!(email).match(emailReg)) {
-      alert("Invalid Email...!!!!!!");
-      e.preventDefault();
-      } else {
-      alert("Form Submitted Successfully......");
-      }
 
-    $('form.new_form').submit(function(event) {
-    event.preventDefault();
-    var $target = $(event.target);
+      $("#new_post_button").on("click", function() {
+      $("#new_form").dialog("open");
+      });
 
-    $.ajax({
-      type: "POST",
-      url: $target.attr('action'),
-      data: $target.serialize()
-    }).done(function(response) {
-      $('#content').append(response);
-      $target.find('textarea').val('');
-      $target.find("input[type=submit]").val('Reply');
-      $('html,body').animate({
-        scrollTop: $('.comment-body:last').offset().top
-      }, 1750);
-    })
-  });
+
+
+      // not implemented
+  //   $('form.new_form').submit(function(event) {
+  //   event.preventDefault();
+  //   var $target = $(event.target);
+
+  //   $.ajax({
+  //     type: "POST",
+  //     url: $target.attr('action'),
+  //     data: $target.serialize()
+  //   }).done(function(response) {
+  //     $('#content').append(response);
+  //     $target.find('textarea').val('');
+  //     $target.find("input[type=submit]").val('Reply');
+  //     $('html,body').animate({
+  //       scrollTop: $('.comment-body:last').offset().top
+  //     }, 1750);
+  //   })
+  // });
 
   $('form.delete_form').submit(function(event) {
     event.preventDefault();
@@ -54,6 +43,4 @@ $(document).ready(function() {
       $target.parent().remove();
     })
   });
-
-
 });
